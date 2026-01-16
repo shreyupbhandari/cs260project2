00fd96abb15689df5687ff5997a67b3a45329f98
shreyup.bhandari <shreyupbhandari@gmail.com>
2026-01-16 12:17:55 -0500

    Just some basic data fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [Grant, setGrant]=useState([]);
+  useEffect(()=>{
+    async function fetchGrant() {
+      const url="/NEH2020sGrant_Short.json";
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+      setGrant(result.Grant);
+    }
+   fetchGrant();
+  },[])
+  
+    
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
