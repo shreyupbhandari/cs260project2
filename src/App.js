eb674c599f59f9fa57744a01898d32e821592783
Shreyup Bhandari <shreyupbhandari@gmail.com>
2026-01-16 19:57:45 -0500

    Printed the small table.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+function TableRow({grantData})
+{
+
+  return (
+    <>
+    <tr>
+      <td> {grantData.ProjectTitle}</td>
+      <td> {grantData.Institution}</td>
+    </tr>
+    </>
+  );
+
+}
+
+function Table({grantArray})
+{
+  return(<table>
+    <tr>
+      <th>Project Title</th>
+      <th>Institution</th>
+    </tr>
+    {grantArray.map(data=>(<TableRow grantData={data}></TableRow>))}
+  </table>);
+
+}
+
+
+
 function App() {
   const [Grant, setGrant]=useState([]);
   useEffect(()=>{
@@ -9,34 +37,17 @@ function App() {
       const url="/NEH2020sGrant_Short.json";
       const response = await fetch(url);
       const result = await response.json();
-      console.log(result);
-      setGrant(result.Grant);
+      setGrant(result.Grants.Grant);
     }
    fetchGrant();
   },[])
   
     
-  
-
-
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Table grantArray={Grant}/>
+
   );
 }
 
